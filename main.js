bba19107b423ccec24c75af0f9e807828e75a45f
santiaguero7 <aguero17.2001@gmail.com>
2025-03-31 21:00:02 -0300

    Se completo la pagina/Se incluyo optimizacion para celular

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-document.querySelector('.menu-btn').addEventListener('click', () => { document.querySelector('.nav-menu').classList.togglee('show');
- });
+document.querySelector(".menubtn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.newcards', { delay: 300 });
+  ScrollReveal().reveal('.bannercard', { delay: 300 });
+  ScrollReveal().reveal('.bannercard2', { delay: 300 });
+ 
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
